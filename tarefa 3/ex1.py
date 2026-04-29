568cbe36a7dfc272b6ae1e09f0a20ae26cbfb692
Isabella <isabella.o@escolar.ifrn.edu.br>
2026-04-28 21:07:09 -0300

    Tarefa 03 finalizada

--- tarefa 3/ex1.py
+++ tarefa 3/ex1.py
@@ -5,11 +5,11 @@ class Viagem:
         self.__litro=0.0
     def set_destino(self,d):
         self.__destino = d
-    def set_ditancia(self,d):
-        if self.__distancia >=0: self.__distancia = d
+    def set_distancia(self,d):
+        if d >=0: self.__distancia = d
         else: raise ValueError()
     def set_litros(self,d):
-        if self.__distancia >=0: self.__litro = d
+        if d >0: self.__litro = d
         else: raise ValueError()
     def get_destino(self):
         return self.__destino
@@ -22,23 +22,26 @@ class Viagem:
     def __str__(self):
         return f'distancia: {self.__distancia} em KM, litros: {self.__litro} e destino: {self.__destino}'
 
-class UI:
+class ViagemUI:
     @staticmethod
     def main():
-        op = UI.menu()
-        while op!=2:
-            if op == 1: UI.calculo()
+        op = ViagemUI.menu()
+        while op != 2:
+            if op == 1:
+                ViagemUI.calculo()
+            op = ViagemUI.menu()
+
     @staticmethod
     def menu():
-        print("1 - viagem, 2 - fim")
+        print("1 - Calcular, 2 - Fim")
         op = int(input("Informe uma opção: "))
         return op
     @staticmethod
     def calculo():
         v=Viagem()
         v.set_destino(input("Informe o destino: "))
-        v.set_ditancia(float(input('Informe a distancia em km: ')))
+        v.set_distancia(float(input('Informe a distancia em km: ')))
         v.set_litros(float(input('Informe a quantidade de litros gastos:')))
+        print(v)
         print(f'Seu consumo médio foi de {v.consumo_m()}')
-        print(Viagem())
-UI.main()
+ViagemUI.main()
